b820326135924445f21faa77bbb9cb97400b975a
Timu5 <timusziomek@gmail.com>
2022-01-27 20:11:56 +0100

    Add benchmark 2

---
+++ flutter/lib/bench2.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'widgets/frame.dart';
+
+class Bench2 extends StatefulWidget {
+  const Bench2({Key? key}) : super(key: key);
+
+  @override
+  State<Bench2> createState() => _Bench2State();
+}
+
+class _Bench2State extends State<Bench2> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Frame(
+      text: "List",
+      topText: "2/2",
+      child: ListView.builder(
+        itemCount: 1000,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: SizedBox(
+                height: 60,
+                width: 200,
+                child: Card(
+                    margin: const EdgeInsets.symmetric(horizontal: 60),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [Text(index.toString())]))),
+          );
+        },
+      ),
+    );
+  }
+}
